693fac45cba3bade915dc60acd7c4f99e8f21468
Liam <liam@liamco.io>
2025-09-01 17:40:06 +0100

    convert to ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App(): React.JSX.Element {
+  return (
+    <>
+      <h1>prospie is coming soon!</h1>
+      <h2>watch this space</h2>
+    </>
+  )
+}
+
+export default App
